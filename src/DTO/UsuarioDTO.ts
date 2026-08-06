f3306d9e49a857a8ef874f30d2c031d9795f369f
Matty-23 <matu.naser@gmail.com>
2026-08-06 10:08:11 -0300

    Se definio tema roles y se completaron los dto

--- src/DTO/UsuarioDTO.ts
+++ src/DTO/UsuarioDTO.ts
@@ -21,3 +21,81 @@ export class ObtenerUsuarioDTO{
     readonly departamento!: string;
 
 }
+
+export class CrearUsuarioDTO{
+    @IsString()
+    @IsNotEmpty()
+    readonly nombre!: string;
+
+    @IsString()
+    @IsNotEmpty()
+    readonly apellido!: string;
+
+    @IsString()
+    @IsNotEmpty()
+    readonly correo!: string;
+
+    @IsString()
+    @IsNotEmpty()
+    readonly contraseña!: string;
+
+    @IsString()
+    @IsNotEmpty()
+    readonly departamento!: string;
+
+    @IsString()
+    @IsNotEmpty()
+    readonly rol!: string;
+}
+
+export class ActualizarUsuarioDTO{
+    @IsNumber()
+    @IsNotEmpty()
+    readonly id!: number;
+
+    @IsString()
+    @IsOptional()
+    readonly nombre?: string;
+
+    @IsString()
+    @IsOptional()
+    readonly apellido?: string;
+
+    @IsString()
+    @IsOptional()
+    readonly correo?: string;
+
+    @IsString()
+    @IsOptional()
+    readonly contraseña?: string;
+
+    @IsString()
+    @IsOptional()
+    readonly departamento?: string;
+
+    @IsString()
+    @IsOptional()
+    readonly rol?: string;
+}
+
+export class ActualizarUsuarioCompletoDTO{
+    @IsNumber()
+    @IsNotEmpty()
+    readonly id!: number;
+
+    @IsString()
+    @IsNotEmpty()
+    readonly nombre!: string;
+
+    @IsString()
+    @IsNotEmpty()
+    readonly apellido!: string;
+
+    @IsString()
+    @IsNotEmpty()
+    readonly correo!: string;
+
+    @IsString()
+    @IsNotEmpty()
+    readonly departamento!: string;
+}
