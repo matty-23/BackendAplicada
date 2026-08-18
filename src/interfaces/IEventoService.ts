5f5fa7b2730a8a7810d2d8bc94f490f2c8b3fc4e
matty-23 <matu.naser@gmail.com>
2026-08-18 19:34:00 -0300

    Revert "Basicamente cambie todo en evento, para que fuera multidia. Cree una …"

--- src/interfaces/IEventoService.ts
+++ src/interfaces/IEventoService.ts
@@ -1,16 +1,15 @@
 import {Evento} from '../models/Evento.js';
 import { filtrosEventoDto } from '../DTO/FiltrosDto.js';
-import { ActualizarEventoDTO } from '../DTO/EventoDto.js';
 export abstract class IEventoService {
     abstract getEventos(page:number): Promise<Evento[]>;
     abstract getEventosActive(page: number): Promise<Evento[]>;
     abstract getEventoById(id: string): Promise<Evento | null>;
     abstract addEvento(evento: Evento): Promise<Evento>;
-    abstract updateDetallesEvento(id: string, dto: ActualizarEventoDTO): Promise<boolean>
+    abstract updateDetallesEvento(evento: Evento): Promise<boolean>;
     abstract deleteEventos(id: string[]): Promise<boolean>
-    abstract agregarParticipantes(idOcurrencia: string, participantes: string[]): Promise<{ advertencia?: string }>;
-    abstract cambiarEncargado(idEvento: string, idOcurrencia: string, idEncargado: string): Promise<boolean> 
-    abstract borrarParticipante(idOcurrencia: string, idUsuario: string): Promise<boolean>;
+    abstract agregarParticipantes(id: string, participantes : string[]): Promise<Evento|null>;
+    abstract cambiarEncargado(idEvento:string, idEncargado:string): Promise<Evento | null>
+    abstract borrarParticipante(idEvento: string, participantes: string): Promise<Evento | null>;
     abstract filtrado(filtros: filtrosEventoDto): Promise<Evento[]>;
 }
 
