bd578d69e7838efe4fc69977c228e609610b6172
Matty-23 <matu.naser@gmail.com>
2026-08-19 01:26:56 -0300

    Se hicieron multiples cambios

--- src/modules/EventoModule.ts
+++ src/modules/EventoModule.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
-
 import { EventoController } from '../controllers/EventoController';
 import { EventoService } from '../services/EventoService';
-
 import { EventoRepository } from '../repository/EventoRepository';
 import { Evento_UsuarioRepository } from '../repository/Evento_UsuarioRepository';
 import { UsuarioRepository } from '../repository/UsuarioRepository';
-
 import { PrismaService } from '../prisma/PrismaService';
+import { AuthGuard } from '../guards/auth.guard';
+import { PermissionsGuard } from '../guards/permissions.guard';
 
 @Module({
   controllers: [
@@ -36,6 +35,9 @@ import { PrismaService } from '../prisma/PrismaService';
       provide: 'IUsuarioRepository',
       useClass: UsuarioRepository,
     },
+
+    AuthGuard,
+    PermissionsGuard,
   ],
 
   exports: [
