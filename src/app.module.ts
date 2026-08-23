7ffd271abb64d9a3d9393c7c5ff896610154a8b5
Amir <cm.nt.amir@gmail.com>
2026-08-23 19:58:46 -0300

    Feature: Modelo solicitud y cambio en BD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsuarioModule } from './modules/UsuarioModule';
 import { EventoModule } from './modules/EventoModule';
+import { SolicitudModule } from './modules/SolicitudModule';
 
 @Module({
   imports: [
     UsuarioModule, 
-    EventoModule
+    EventoModule,
+    SolicitudModule
   ],
   controllers: [],
   providers: [],
